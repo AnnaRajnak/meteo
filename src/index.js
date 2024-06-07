c2d15b2f7a9250a04380e4910f553291618441d6
AnnaRajnak <annarajnakharmanosova@gmail.com>
2024-06-07 12:10:18 +0200

    Added Icon

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ function refreshWeather(response) {
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
 
+  let iconElement = document.querySelector("#icon");
+
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+                alt=""
+              />`;
 }
 function formatDate(date) {
   let hours = date.getHours();

96604c93972c047f985dd3605f6338b3b9f85a71
AnnaRajnak <annarajnakharmanosova@gmail.com>
2024-06-10 16:37:29 +0200

    Forecast Function JS

--- src/index.js
+++ src/index.js
@@ -55,5 +55,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="row">
+            <div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <div class="forecast-icon">
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png"
+                  alt=""
+                  width="36"
+                />
+              </div>
+              <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max"> 18°</span> /
+
+                <span class="weather-forecast-temperature-min"> 12°</span>
+              </div>
+            </div>
+          </div>`;
+  });
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+displayForecast();
